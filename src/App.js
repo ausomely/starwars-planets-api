8d9cd158e9e72d5552cd60e1a0ee947e66f33e34
ausomely <jaackieau@gmail.com>
2021-10-19 16:52:42 -0700

    sort planets, chart config needed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 import './App.css';
-import { useState, useEffect } from 'react';
 import PlanetChart from './components/PlanetChart';
 
-const fetchData = (url) => {
-  const [data, setData] = useState('');
-  // const [loading, setLoading] = useState(true);
-
-  useEffect(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    const [...planetsArray] = data.results;
-    setData(planetsArray);
-    // setLoading(false);
-  }, []);
-
-  // return { data, loading };
-  return { data };
-};
-
 function App() {
-  const { data }  = fetchData('https://swapi.dev/api/planets/'); 
-  console.log(data);
   return (
     <div className="App">
-      {/* {loading ? <div> ...loading </div> : <div>{data.name}</div>} */}
-      <PlanetChart data={data}/>
+      <PlanetChart />
     </div>
   );
 }
--- src/components/PlanetChart.js
+++ src/components/PlanetChart.js
@@ -1,11 +1,73 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
 
-const PlanetChart = ({ data }) => {
+
+const PlanetChart = () => {
+    const [chartData, setChartData] = useState({});
+    const renderChart = (data) => {
+        let planetNames = [];
+        let populations = [];
+        for (const planetObj of Object.values(data)) {
+            planetNames.push(planetObj.name);
+            populations.push(parseInt(planetObj.population));
+        }
+    console.log(planetNames, populations);
+    
+        setChartData({
+            labels: planetNames,
+            datasets: [
+                {
+                    label: 'Planet Population',
+                    data: populations,
+                    backgroundColor: [
+                        'rgba(75, 192, 192, 0.6)'
+                    ],
+                    borderWidth: 4
+                }
+            ]
+        });
+    };
+  
+    const fetchData = (url) => {
+        const [data, setData] = useState([]);
+        const [loading, setLoading] = useState(true);
+        
+
+        useEffect(async () => {
+          const response = await fetch(url);
+          const data = await response.json();
+          const [...items] = data.results;
+
+          // Alphabetically sort planets by name
+          const sortedPlanets = items.sort((a, b) => {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        });
+
+        console.log(sortedPlanets);
+          setData(sortedPlanets);
+          setLoading(false);
+          renderChart(sortedPlanets);
+        }, []);
+      
+        return { data, loading };
+    };
+
+    const {data, loading} = fetchData('https://swapi.dev/api/planets/');
+
     return (
         <div>
-            {Object.values(data).map((planet, planetIdx) => (
+            {loading ? <div> ...loading </div> : Object.values(data).map((planet, planetIdx) => (
                 <p key={planetIdx}>{planet.name} {planet.population}</p>
             ))}
+            <div>
+                <Bar 
+                data={chartData} 
+                />
+            </div>
         </div>
     )
 }
